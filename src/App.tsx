3d84ec0e28ce78c9fadfe33e9d015f671349ab4e
Ben Schlegel <ben5.schlegel@gmail.com>
2021-09-09 20:02:09 +0200

    Broken dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { registerRootComponent } from 'expo';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { Header } from '../components/Header';
+import uuid from 'uuidv4';
 
 function App(): React.ReactElement {
+  const [count, setCount] = useState(7);
+
   return (
     <View style={styles.container}>
       <Header title="Shopping List" />
+      <Text>{count}</Text>
     </View>
   );
 }
