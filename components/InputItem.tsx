26211c674434f6d4b22555071053a0ebf6dc18b5
Ben Schlegel <ben5.schlegel@gmail.com>
2021-09-11 20:40:22 +0200

    Add keyboardavoiding view, implement InputItem

--- components/InputItem.tsx
+++ components/InputItem.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState, useRef } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity, Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import changeNavigationBarColor from 'react-native-navigation-bar-color';
 
 interface Props {
   addItem: (text: string) => void;
 }
 
 export function InputItem({ addItem }: Props): React.ReactElement {
+  //changeNavigationBarColor('#80b3ff', true, true); //crashes
+
   const [text, setText] = useState('');
   const inputField = useRef<null | TextInput>(null);
 
@@ -20,32 +23,56 @@ export function InputItem({ addItem }: Props): React.ReactElement {
   };
 
   return (
-    <View style={styles.bottom}>
-      <TextInput
-        placeholder="Add Item..."
-        placeholderTextColor="#FFF"
-        style={styles.input}
-        onChangeText={onTextChange}
-        ref={inputField}
-        onSubmitEditing={() => {
-          if (text.trim() !== '') submitInput(text);
-        }}
-      />
-    </View>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.containerBottom}>
+      <Ionicons name={'chevron-forward-outline'} size={28} color={'#c2bad8'} style={styles.icon} />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Add Item..."
+          placeholderTextColor="#c2bad8"
+          style={styles.input}
+          onChangeText={onTextChange}
+          ref={inputField}
+          onSubmitEditing={() => {
+            if (text.trim() !== '') submitInput(text);
+          }}
+        />
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  input: {
-    height: 60,
-    padding: 8,
-    fontSize: 16,
+  icon: {
+    marginLeft: 8,
+    marginTop: 6,
+    alignSelf: 'center',
   },
-  bottom: {
+  input: {
+    marginLeft: 5,
+    height: 60,
+    paddingBottom: 30,
+    fontSize: 16,
+    color: '#c2bad8',
+  },
+  inputContainer: {
+    height: 50,
+    flex: 1,
+    marginBottom: 8,
+    marginRight: 25,
+    marginTop: 15,
+    marginLeft: 6,
+    // borderRadius: 8,
+    // borderColor: '#FFF',
+    // borderWidth: 2,
+  },
+  containerBottom: {
     borderTopStartRadius: 12,
     borderTopEndRadius: 12,
+    overflow: 'hidden',
     flex: 0.1,
     backgroundColor: '#322775',
-    justifyContent: 'flex-end',
+    justifyContent: 'flex-start',
+    alignContent: 'space-between',
+    flexDirection: 'row',
   },
 });
